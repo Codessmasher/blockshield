430b7cc7eb9cf7dfcd7336a8c262c717d92a7f59
Deepjyoti Das <deepdeepdasdasdas@gmail.com>
2023-11-01 20:10:17 +0530

    Edited readme

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const press = (skipMyClass) => {
     const buttons = document.getElementsByClassName(skipMyClass);
     for (const button of buttons) {
-        button.click(); 
+        button.click();
     }
 }
 
-setInterval(() => { 
+setInterval(() => {
     press("ytp-ad-skip-button-text");
     press("ytp-ad-overlay-close-button");
-
 }, 200);
+
+// Remove the "ad-created" class (if it exists)
+const adCreatedElement1 = document.querySelector(".ad-created");
+if (adCreatedElement1) {
+    adCreatedElement1.classList.remove("ad-created");
+}
+
+// Remove the "video-ads , ytp-ad-module" class (if it exists)
+const adCreatedElement2 = document.querySelector(".video-ads");
+if (adCreatedElement2) {
+    adCreatedElement2.classList.remove("video-ads");
+}
+
+const adCreatedElement3 = document.querySelector(".ytp-ad-module");
+if (adCreatedElement3) {
+    adCreatedElement3.classList.remove("ytp-ad-module");
+}
 
 console.log("Created by Deepjyoti Das");
